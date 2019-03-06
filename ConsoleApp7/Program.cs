5d531b0d9beb9e5eeaafdb0741435d35b3a877bf
visualstudio <visualstudio@122-PC25>
2019-03-06 11:33:38 -0500

    final7

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -10,44 +10,43 @@ using System.Threading.Tasks;
     //Gagan Sidhu c0731555
     //CSD3354 Section 2
     //March 6,2019
-   
+
+    public class Program
     {
-        public class Program
+        public static void Main()
         {
-            public static void Main()
-            {
-                DelegateExercises delegateExercises = new DelegateExercises();
-                delegateExercises.Method3();
-                Console.ReadLine();
-            }
-        }
-
-        public class DelegateExercises
-        {
-            public delegate int MyDelegate(int intValue);
-
-            int Method1(int intMethod1)
-            {
-                return intMethod1 * 2;
-            }
-
-            int Method2(int intMethod1)
-            {
-                return intMethod1 * 10;
-            }
-
-            public void Method4(MyDelegate myDelegate)
-            {
-                int result = myDelegate(10);
-                Console.WriteLine(result);
-            }
-
-            public void Method3()
-            {
-                MyDelegate myDelegate = new MyDelegate(Method1);
-                Method4(myDelegate);
-                myDelegate = new MyDelegate(Method2);
-                Method4(myDelegate);
-            }
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
+            Console.ReadLine();
         }
     }
+
+    public class DelegateExercises
+    {
+        public delegate int MyDelegate(int intValue);
+
+        int Method1(int intMethod1)
+        {
+            return intMethod1 * 4;
+        }
+
+        int Method2(int intMethod1)
+        {
+            return intMethod1 * 20;
+        }
+
+        public void Method4(MyDelegate myDelegate)
+        {
+            for (int i = 1; i <= 5; i++)
+                System.Console.Write(myDelegate(i) + " ");
+        }
+
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            Method4(myDelegate);
+            myDelegate = new MyDelegate(Method2);
+            Method4(myDelegate);
+        }
+    }
+}
