fe0d2a6241ab411bcc148f1f8c490104e6f99827
visualstudio <visualstudio@122-PC25>
2019-03-06 11:41:32 -0500

    lab 12

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -10,13 +10,39 @@ using System.Threading.Tasks;
     //Gagan Sidhu c0731555
     //CSD3354 Section 2
     //date 06 march 2019
-    public delegate void MyDelegate();
+    using System;
 
-    public class DelegateExercises
+    delegate void ExampleDelegate(string xyz);
+
+    class Program
     {
-        void Method3()
+        public static void Method1(string xyz)
         {
-            System.Console.WriteLine(MyDelegate.ToString());
+            Console.WriteLine(xyz + " Method1");
+        }
+
+        public static void Method2(string xyz)
+        {
+            Console.WriteLine(xyz + " Method2");
+        }
+
+        public static void Main()
+        {
+            ExampleDelegate ex1Delegate, ex2Delegate, ex3Delegate, myDelegate;
+
+            ex1Delegate = new ExampleDelegate(Method1);
+            ex2Delegate = new ExampleDelegate(Method2);
+            ex3Delegate = ex1Delegate + ex2Delegate;
+            myDelegate = ex1Delegate - ex2Delegate;
+            ex1Delegate("AAA");
+            ex2Delegate("BBB");
+            ex3Delegate("CCC");
+            myDelegate("DDD");
+            myDelegate = ex3Delegate - ex1Delegate;
+            myDelegate("EEE");
+            myDelegate = ex3Delegate - ex2Delegate;
+            myDelegate("FFF");
+            Console.ReadLine();
         }
     }
-    
+}
