7af6e9b56d794ff2586a183d14ff8437e71d1b91
visualstudio <visualstudio@122-PC25>
2019-03-06 11:29:04 -0500

    final5

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -4,12 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp7
+        namespace DelegatesAndEvents
 {
-    class Program
-    {
-        static void Main(string[] args)
+    // Navita Navita c0730283
+    //Gagan Sidhu c0731555
+    //CSD3354 Section 2
+    //March 6,2019
+    public class Program
         {
+            public static void Main()
+            {
+                DelegateExercises delegateExercises = new DelegateExercises();
+                delegateExercises.Method3();
+            }
         }
+
+        public class DelegateExercises
+        {
+            public delegate int MyDelegate();
+
+            void Method1()
+            {
+                System.Console.WriteLine("MyDelegate");
+            }
+
+            public void Method3()
+            {
+                MyDelegate myDelegate = new MyDelegate(Method1);
+                myDelegate();
+            }
+        }
+    }
+}
     }
 }
